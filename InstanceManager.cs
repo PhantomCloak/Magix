23074230f61c388148e01e228ee415d846961354
PhantomCloak <unalozyurtrooter@hotmail.com>
2024-01-19 01:58:14 +0300

    replace static instation with reflection

--- InstanceManager.cs
+++ InstanceManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 namespace Magix
 {
     public static class InstanceManager
@@ -10,7 +13,18 @@ namespace Magix
             get
             {
                 if (m_ResourceAPI == null)
-                    return new CloudApiWrapper();
+                {
+                    var apiType = AppDomain.CurrentDomain.GetAssemblies()
+                        .SelectMany(assembly => assembly.GetTypes())
+                        .FirstOrDefault(type => type.IsClass && !type.IsAbstract && typeof(ICloudResourceAPI).IsAssignableFrom(type));
+
+                    if (apiType != null)
+                    {
+                        m_ResourceAPI = (ICloudResourceAPI)Activator.CreateInstance(apiType);
+						Logger.LogVerbose($"Created an instance of {apiType.FullName} for ICloudResourceAPI.");
+                    }
+                }
+
                 return m_ResourceAPI;
             }
             set => m_ResourceAPI = value;
