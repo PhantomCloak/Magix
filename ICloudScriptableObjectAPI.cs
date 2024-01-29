0f0c96385ab1a33c639ab4c85f024a6ec306a154
PhantomCloak <unalozyurtrooter@hotmail.com>
2024-01-29 15:21:14 +0300

    refactor interface

--- ICloudScriptableObjectAPI.cs
+++ ICloudScriptableObjectAPI.cs
@@ -12,14 +12,14 @@ namespace Magix
         public string EditorUserId { get; }
 
         // Success, Result
-        public void CheckVariableIsExist(string userId, string resourceName, Action<bool, bool> callback);
+        public void CheckVariableIsExist(string userId, string resourceName, Action<bool> callback);
         // Success, ErrorMessage, Result
-        public void GetVariableCloudJson(string variableName, Type type, Action<bool, string, string> onCompleteCallback);
+        public void GetVariableCloudJson(string variableName, Type type, Action<bool, string> onCompleteCallback);
         // Success, ErrorMessage, Result
-        public void SetVariableCloud<T>(string variableName, T obj, Action<bool, string> onCompleteCallback = null);
+        public void SetVariableCloud<T>(string variableName, T obj, Action<bool> onCompleteCallback = null);
         // Success, Result
-        public void GetAllEntriesUser(string userName, Action<bool, Dictionary<string, string>> onCompleteCallback);
+        public void GetAllEntriesUser(string userName, Action<Dictionary<string, string>> onCompleteCallback);
         // Succes, Message
-        public void DeleteVariableCloud(string variableName, Action<bool, string> onCompleteCallback = null);
+        public void DeleteVariableCloud(string variableName, Action<bool> onCompleteCallback = null);
     }
 }
